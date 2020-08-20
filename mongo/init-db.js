0b136210187ed2aa765d57b4e8996eeebd61ec82
Nathan Asselstine <nate.asselstine@gmail.com>
2020-08-20 19:39:05 -0400

    feat: Convert ID to number, init DB validation

--- mongo/init-db.js
+++ mongo/init-db.js
@@ -7,12 +7,13 @@
  \******************************************************************************/
 
 // Create our Pokedex database.
-databases = ["pokedex"];
+databases = ["pokedex", "log"];
 
 // Switch to the Pokedex DB
 db = db.getSiblingDB("pokedex");
+log = db.getSiblingDB("log")
 
-// Create our Pokedex application user
+// Create our Pokedex user/role
 db.createUser({
     "user": "ash",
     "pwd": "ketchum",
@@ -23,15 +24,64 @@ db.createUser({
         }
     ]
 });
+// Create our log user/role
+log.createUser({
+    "user": "ash",
+    "pwd": "ketchum",
+    "roles": [
+        {
+            "role": "readWrite",
+            "db": "log"
+        }
+    ]
+});
 
 // Create the collection of Pokemon in our Pokedex
 db.createCollection("pokemon");
 
-// Load the pokemons.json into a JSON object
-const fileContent = cat("/docker-entrypoint-initdb.d/pokemons.json");
-const pokemon = JSON.parse(fileContent);
+// Create a collection of warning and error events in our log db
+log.createCollection("warnings");
+log.createCollection("errors");
 
-// Insert the data into the collection
-db.pokemon.insertMany(pokemon);
+// Load the pokemons.json into a JSON object
+let pokemonList = JSON.parse(cat("/docker-entrypoint-initdb.d/pokemons.json"));
+
+// Only exists for testing.
+// Uncomment to confirm model validation is working.
+// pokemonList = pokemonList.concat(JSON.parse(cat("/docker-entrypoint-initdb.d/invalid-pokemons.json")));
+
+// Keep track of the known IDs. We don't want to seed duplicates either.
+const insertedIds = [];
+
+// Validate the model. Our only requirement is a numeric ID and a name.
+for (let pokemon of pokemonList) {
+    if (isNaN(pokemon.id) ||
+        pokemon.name === undefined ||
+        pokemon.name === null ||
+        pokemon.name.trim() === "") {
+        // This is an invalid entry. Do not add it to the collection.
+        // Log the failure to a separate database collection.
+        log.errors.insertOne({
+            "time": new Date(),
+            "message": `Encountered invalid Pokemon during startup. ID: '${pokemon.id}', Name: '${pokemon.name}'`
+        });
+    } else {
+        // Convert the ID to be a number
+        pokemon.id = Number(pokemon.id);
+
+        // Have we encountered this Pokemon yet?
+        if (insertedIds.includes(pokemon.id)) {
+            log.warnings.insertOne({
+                "time": new Date(),
+                "message": `Encountered duplicate Pokemon during startup. ID: '${pokemon.id}', Name: '${pokemon.name}'`
+            });
+        } else {
+            // Add to the list of added Pokemon
+            insertedIds.push(pokemon.id);
+            // Insert into the database collection
+            db.pokemon.insertOne(pokemon);
+        }
+    }
+}
 
 // TODO: catch 'em all
