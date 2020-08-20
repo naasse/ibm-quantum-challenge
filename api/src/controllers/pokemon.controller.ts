b81931c47acad943c45696c8fe43ec04ac383c03
Nathan Asselstine <nate.asselstine@gmail.com>
2020-08-19 23:13:27 -0400

    feat: Initial GET path parameter

--- api/src/controllers/pokemon.controller.ts
+++ api/src/controllers/pokemon.controller.ts
@@ -1,10 +1,23 @@
-import {get} from '@loopback/rest';
+import {get, param} from '@loopback/rest';
 
 export class PokemonController {
     @get('/pokemon')
-    getPokemon(): object[] {
+    listPokemon(): object[] {
         // TODO - retrieve from database
-        const pokemon = [{"id": 1}];
+        // TODO - allow pagination
+        // TODO - allow filter on type, favorites only, or if possible, any field
+        const pokemonList = [{"id": 1}];
+        return pokemonList;
+    }
+
+    @get('/pokemon/{id}')
+    getPokemon(@param.path.string('id') id: string): object {
+        // TODO - retrieve from database
+        const pokemon = {"id": id};
         return pokemon;
     }
+
+    // TODO - Query a pokemon by name (Is this different than searching on name?)
+    // TODO - Query list of pokemon types
+    // TODO - Mutation to mark/unmark pokemon as favorite
 }
